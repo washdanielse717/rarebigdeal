010ec7527bc15f868c4c3a05212edd7e9913ba0d
Dan Mindru <mindrudan@gmail.com>
2024-11-20 08:27:53 +0100

    Layout changes

--- shipixen/app/categories/layout.tsx
+++ shipixen/app/categories/layout.tsx
@@ -8,7 +8,7 @@ const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
     <div className="w-full flex flex-col items-center min-h-screen">
       <Header className="mb-4 lg:mb-4" />
       <main className="w-full flex flex-col md:flex-row justify-center gap-6 flex-grow container-wide px-4">
-        <Menu className="w-full md:max-w-[300px] md:sticky top-6 overflow-auto max-h-[calc(100vh-4rem)]" />
+        <Menu className="w-full md:max-w-[300px] md:sticky top-6 md:overflow-auto md:max-h-[calc(100vh-4rem)]" />
         {children}
       </main>
       <Footer />
