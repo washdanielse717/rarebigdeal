4c0f3b29ff038e9830d3ca42a5182928938b7e8b
Dan Mindru <mindrudan@gmail.com>
2024-11-23 18:59:31 +0100

    Add Sentry 🔼

---
+++ shipixen/sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://8cb89fa8cede564393dff12c7ff498ad@o4507021808893952.ingest.us.sentry.io/4508348802007041",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
