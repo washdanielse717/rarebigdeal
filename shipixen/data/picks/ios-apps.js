76d4f53f5ce074764b818619dbe6b200cdbc6017
Dan Mindru <mindrudan@gmail.com>
2024-11-25 21:26:39 +0100

    Add iOS picks

---
+++ shipixen/data/picks/ios-apps.js
@@ -0,0 +1,11 @@
+module.exports = {
+  name: 'Best iOS Apps',
+  description: 'iOS apps that will keep your phone in your hand.',
+  apps: [
+    'Things3',
+    'Paprika Recipe Manager 3',
+    'TimeLapsed',
+    'Atomic',
+    'Inventory Tracker Itemlist',
+  ],
+};
